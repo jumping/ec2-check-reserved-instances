750c328f0bd89f9018302cafb9297ec251555426
jumping <quzhengping@gmail.com>
2013-03-28 09:35:24 +0800

    squezze output

--- ec2-check-reserved-instances.py
+++ ec2-check-reserved-instances.py
@@ -24,7 +24,7 @@ from boto.manage import propget
 #		print "Please set env variable"
 #		sys.exit(1)
 
-
+DETAILS=False
 #ec2_conn = boto.connect_ec2(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 
 def get_region():
@@ -48,9 +48,9 @@ running_instances = {}
 for reservation in reservations:
     for instance in reservation.instances:
         if instance.state != "running":
-            sys.stderr.write("Disqualifying instance %s: not running\n" % ( instance.id ) )
+            if DETAILS: sys.stderr.write("Disqualifying instance %s: not running\n" % ( instance.id ) )
         elif instance.spot_instance_request_id:
-            sys.stderr.write("Disqualifying instance %s: spot\n" % ( instance.id ) )
+            if DETAILS: sys.stderr.write("Disqualifying instance %s: spot\n" % ( instance.id ) )
         else:
             if instance.vpc_id:
                 print "Does not support vpc yet, please be careful when trusting these results"
@@ -66,12 +66,12 @@ remove_zero(running_instances)
 
 reserved_instances = {}
 for reserved_instance in ec2_conn.get_all_reserved_instances():
-	if reserved_instance.state != "active":
-		sys.stderr.write( "Excluding reserved instances %s: no longer active\n" % ( reserved_instance.id ) )
-	else:
-		az = reserved_instance.availability_zone
-		instance_type = reserved_instance.instance_type
-		reserved_instances[( instance_type, az) ] = reserved_instances.get ( (instance_type, az ), 0 )  + reserved_instance.instance_count
+    if reserved_instance.state != "active":
+        if DETAILS: sys.stderr.write( "Excluding reserved instances %s: no longer active\n" % ( reserved_instance.id ) )
+    else:
+        az = reserved_instance.availability_zone
+        instance_type = reserved_instance.instance_type
+        reserved_instances[( instance_type, az) ] = reserved_instances.get ( (instance_type, az ), 0 )  + reserved_instance.instance_count
 
 #prune reserved_instances
 remove_zero(reserved_instances)
@@ -86,26 +86,26 @@ instance_diff = dict([(x, reserved_instances[x] - running_instances.get(x, 0 )) 
 
 # instance_diff only has the keys that were present in reserved_instances. There's probably a cooler way to add a filtered dict here
 for placement_key in running_instances:
-	if not placement_key in reserved_instances:
-		instance_diff[placement_key] = -running_instances[placement_key]
+    if not placement_key in reserved_instances:
+        instance_diff[placement_key] = -running_instances[placement_key]
 
 # pprint ( instance_diff )
 
 unused_reservations = dict((key,value) for key, value in instance_diff.iteritems() if value > 0)
 if unused_reservations == {}:
-	print "Congratulations, you have no unused reservations"
+    print "Congratulations, you have no unused reservations"
 else:
-	for unused_reservation in unused_reservations:
-		print "UNUSED RESERVATION!\t(%s)\t%s\t%s" % ( unused_reservations[ unused_reservation ], unused_reservation[0], unused_reservation[1] )
+    for unused_reservation in unused_reservations:
+        print "UNUSED RESERVATION!\t(%s)\t%s\t%s" % ( unused_reservations[ unused_reservation ], unused_reservation[0], unused_reservation[1] )
 
 print ""
 
 unreserved_instances = dict((key,-value) for key, value in instance_diff.iteritems() if value < 0)
 if unreserved_instances == {}:
-	print "Congratulations, you have no unreserved instances"
+    print "Congratulations, you have no unreserved instances"
 else:
-	for unreserved_instance in unreserved_instances:
-		print "Instance not reserved:\t(%s)\t%s\t%s" % ( unreserved_instances[ unreserved_instance ], unreserved_instance[0], unreserved_instance[1] )
+    for unreserved_instance in unreserved_instances:
+        print "Instance not reserved:\t(%s)\t%s\t%s" % ( unreserved_instances[ unreserved_instance ], unreserved_instance[0], unreserved_instance[1] )
 
 qty_running_instances = reduce( lambda x, y: x+y, running_instances.values() )
 qty_reserved_instances = reduce( lambda x, y: x+y, reserved_instances.values() )
